c22b033e4554b1e881bdb8512553fef984e9bc01
omiras <oscar.miras@gmail.com>
2022-06-16 18:46:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gatos y Perros
+
+Vamos a crear una pequeña que nos muestre los perros y gatos, en función de la categoría que el usuario seleccione.
+
+## Req 1
+
+- Si el usuario introduce la ruta /category/cats
+  - Vamos a mostrar todos los gatos del array cats
+- Si el usuario introduce la ruta /category/dogs
+  - Vamos a mostrar todos los perros del array dogs
---
+++ app.js
@@ -0,0 +1,102 @@
+const cats = [
+	{
+		"name": "Persian cat",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/cat/images/persian_cat.jpg"
+	},
+
+	{
+		"name": "Russian Blue",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/cat/images/russian_blue.jpg"
+	},
+
+	{
+		"name": "Bengal cat",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/cat/images/bengal_blue.jpeg"
+	},
+
+	{
+		"name": "British Shorthair",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/cat/images/british_shorthairs.jpeg"
+	},
+
+	{
+		"name": "Maine Coon",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/cat/images/maine_coon.jpg"
+	}
+];
+
+const dogs = [
+
+	{
+		"name": "Labrador Retriever",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/dog/images/labrador_retriver.jpeg"
+	},
+
+	{
+		"name": "Pug",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/dog/images/pug.jpg"
+	},
+
+	{
+		"name": "Siberian Husky",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/dog/images/siberian_husky.jpg"
+	},
+
+	{
+		"name": "Beagle",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/dog/images/beagle.jpg"
+	},
+
+	{
+		"name": "Pomeranian",
+		"image": "https://raw.githubusercontent.com/haseebpvt/Animal-API/master/data/dog/images/pomeranian.jpeg"
+	},
+
+]
+
+// cómo se gestionan las rutas con Express
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+// Definir rutas
+
+app.get("/", mostrarHomePage);
+
+app.get("/category/:type", (req, res) => {
+	// En el objeto 'params' tenemos todos los parámetros dinámicos que nos ha pasado el cliente
+	const type = req.params.type;
+
+	if (type == "cats") {
+		res.render("animals", {
+			animalType: 'GATOS',
+			animals: cats
+		});
+	}
+
+	else if (type == "dogs") {
+		res.render("animals", {
+			animalType: 'PERROS',
+			animals: dogs
+		});
+	}
+
+	else {
+		res.render("animals", {
+			animalType: 'NO TENEMOS ESTA CATEGORÍA',
+			animals: []
+		});
+	}
+});
+
+function mostrarHomePage(req, res) {
+	res.render('index');
+}
+
+app.use((req, res) => {
+	res.status(404).render('404');
+})
+
+app.listen(3000);
+
